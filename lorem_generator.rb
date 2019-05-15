762a273e58313542039e3cf8d62a53525defeb1c
Marcos Villegas <dinavers@gmail.com>
2019-05-14 21:24:19 -0400

    lorem generator

---
+++ lorem_generator.rb
@@ -0,0 +1,5 @@
+ARGV[0].to_i
+
+ARGV[0].to_i.times do |i|
+puts 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur felis nisl, maximus in pulvinar sed, pretium non turpis. Aliquam tempor iaculis mauris at euismod. Morbi semper tristique placerat. Praesent mollis ipsum non ullamcorper pulvinar. Sed neque tortor, auctor sit amet interdum quis, aliquam accumsan justo. Maecenas nec cursus nisl. Donec et mi vitae sapien posuere sagittis. Donec sagittis sapien neque, sed ullamcorper metus accumsan nec. Praesent tincidunt id elit non luctus.'
+end
